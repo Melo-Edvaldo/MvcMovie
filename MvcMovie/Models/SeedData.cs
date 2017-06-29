c4b86dd2a6dd730580e0c5c2ffd4a6a25b5d1cee
Edvaldo de Oliveira Melo <edvaldomelo@byui.edu>
2017-06-29 01:53:36 -0300

    Codes Part 2

---
+++ MvcMovie/Models/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                     new Movie
+                     {
+                         Title = "The Other Side of Heaven",
+                         ReleaseDate = DateTime.Parse("2001-12-14"),
+                         Genre = "Adventure, Biography, Drama",
+                         Rating = "R",
+                         Price = 20.09M
+                     }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
